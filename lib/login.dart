9ce8eba8c1839ea1038554b305857b53d81c8c61
phquang98 <53476449+phquang98@users.noreply.github.com>
2024-01-03 10:52:52 +0700

    feat: add btn placeholder

--- lib/login.dart
+++ lib/login.dart
@@ -15,15 +15,12 @@ class _LoginScreenState extends State<LoginScreen> {
       children: <Widget>[
         Expanded(child: Container(child: Image.asset('assets/logo_evn.png'))),
         Expanded(
-            //   child: Row(children: [Text(
-            // 'xaaxaxaxa',
-            // style: TextStyle(color: Colors.blue[900], fontSize: 20),],)
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [
+              children: <Widget>[
                 Text(
                   'Don\'t have an account yet ?',
                   style: TextStyle(color: Colors.blue[900], fontSize: 20),
@@ -36,7 +33,81 @@ class _LoginScreenState extends State<LoginScreen> {
                       fontWeight: FontWeight.bold),
                 ),
               ],
-            )
+            ),
+            Expanded(
+                child: Padding(
+                    padding:
+                        const EdgeInsets.only(left: 40, right: 40, top: 10),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Container(
+                            color: Colors.brown[100],
+                            child: Row(
+                              children: <Widget>[
+                                Padding(
+                                    padding: const EdgeInsets.all(20),
+                                    child: Icon(Icons.accessibility,
+                                        color: Colors.blue[900])),
+                                Text(
+                                  'E M A I L',
+                                  style: TextStyle(
+                                      color: Colors.blue[900], fontSize: 20),
+                                )
+                              ],
+                            ))))),
+            Expanded(
+                child: Padding(
+                    padding:
+                        const EdgeInsets.only(left: 40, right: 40, top: 10),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Container(
+                            color: Colors.brown[100],
+                            child: Row(
+                              children: <Widget>[
+                                Padding(
+                                    padding: EdgeInsets.all(20),
+                                    child: Icon(Icons.lock,
+                                        color: Colors.blue[900])),
+                                Text('P A S S W O R D',
+                                    style: TextStyle(
+                                        color: Colors.blue[900], fontSize: 20)),
+                              ],
+                            ))))),
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Forgot Password ?',
+                    style: TextStyle(color: Colors.blue[900], fontSize: 20),
+                  ),
+                  Text(
+                    ' SIGN UP',
+                    style: TextStyle(
+                        color: Colors.blue[900],
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+                child: Padding(
+                    padding:
+                        const EdgeInsets.only(left: 40, right: 40, top: 10),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Container(
+                            color: Colors.blue[900],
+                            child: const Center(
+                              child: Text('S I G N  I N',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold)),
+                            ))))),
           ],
         ))
       ],
